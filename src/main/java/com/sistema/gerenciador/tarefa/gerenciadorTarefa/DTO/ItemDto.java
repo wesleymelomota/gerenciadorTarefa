d5c8b6ce885ac0d0e5cd8150f5bb8554e79d007b
Wesley Melo mota <wesleymota382@gmail.com>
2024-09-11 21:17:01 -0300

    add anotation allArgConstructor

--- src/main/java/com/sistema/gerenciador/tarefa/gerenciadorTarefa/DTO/ItemDto.java
+++ src/main/java/com/sistema/gerenciador/tarefa/gerenciadorTarefa/DTO/ItemDto.java
@@ -1,9 +1,11 @@
 package com.sistema.gerenciador.tarefa.gerenciadorTarefa.DTO;
 
 import com.sistema.gerenciador.tarefa.gerenciadorTarefa.Enums.EstadoItem;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ItemDto {
     private Integer id;
     private String titulo;
