bcfee88269dbf50406be145650ccfe40d9704fc5
Wesley Melo mota <wesleymota382@gmail.com>
2024-09-12 22:46:52 -0300

    criando novo dto

---
+++ src/main/java/com/sistema/gerenciador/tarefa/gerenciadorTarefa/DTO/ListaDtoUpdate.java
@@ -0,0 +1,11 @@
+package com.sistema.gerenciador.tarefa.gerenciadorTarefa.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ListaDtoUpdate {
+    private Integer id;
+    private String nome;
+}
